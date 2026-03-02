dc3be7bb6a425034069d3b22b6a245a5230ab0ff
Gabo-Dev <jonathanornaponce@gmail.com>
2026-03-02 17:07:51 +0100

    feat(cliente): implement multi-resource competition using select

--- internal/cliente/cliente.go
+++ internal/cliente/cliente.go
@@ -27,14 +27,20 @@ func (cli *Cliente) Vivir(c *centro.Centro){
 
 	fmt.Printf(">> Cliente %d ha llegado para una sesión de %s.\n", cli.ID, cli.Tipo)
 
-	fmt.Printf(" ...Cliente  %d está en la cola, esperando un masajista...\n",cli.ID)
-	<-c.MasajistaDisponible
-  fmt.Printf("Masajista: Atendiendo al cliente %d\n", cli.ID)
-
-	time.Sleep(2 * time.Second) // Simulamos que está un rato en el centro
-	
-	// liberamos canal
-	c.MasajistaDisponible <- struct{}{}
-	fmt.Printf("   <-- Cliente %d TERMINÓ su masaje y LIBERA al masajista.\n", cli.ID)
+	fmt.Printf(" ...Cliente  %d está en la cola, esperando su turno para ser atendido por un empleado.\n",cli.ID)
+	select {
+		case <-c.MasajistaDisponible:
+  		fmt.Printf("Masajista: Atendiendo al cliente %d\n", cli.ID)
+			time.Sleep(2 * time.Second) // Simulamos que está un rato en el centro
+			// liberamos canal del masajista
+			c.MasajistaDisponible <- struct{}{}
+			fmt.Printf("   <-- Cliente %d TERMINÓ su masaje y LIBERA al masajista.\n", cli.ID)
+		case <-c.FisioDisponible:
+			fmt.Printf("Fisio: Atendiendo al cliente %d\n", cli.ID)
+			time.Sleep(2 * time.Second)
+			// liberamos canal del Fisio
+			c.FisioDisponible <- struct{}{}
+			fmt.Printf("   <-- Cliente %d TERMINÓ su masaje y LIBERA al FISIOTERAPEUTA.\n", cli.ID)
+	}
 	fmt.Printf("<< Cliente %d se ha ido del centro.\n", cli.ID)
 }
